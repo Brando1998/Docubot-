9bb47c50a40444b202cabc38a1e61c1a70a864db
Brando1998 <brandodiazmont@gmail.com>
2025-09-26 08:12:37 -0500

    fix

--- api/models/paseto.go
+++ api/models/paseto.go
@@ -5,7 +5,7 @@ import "time"
 // PasetoPayload define la estructura del payload del token PASETO
 type PasetoPayload struct {
 	UserID    uint      `json:"user_id"`
-	Email     string    `json:"email"`
+	Username  string    `json:"username"`
 	Role      string    `json:"role"`
 	IssuedAt  time.Time `json:"issued_at"`
 	ExpiresAt time.Time `json:"expires_at"`
